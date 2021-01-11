98cf3ecd0530891182de7aedb6b0a434d5ab5f4d
losbiw <skorinov41@gmail.com>
2021-01-11 22:28:38 +0200

    Add content

--- public/scripts/api.js
+++ public/scripts/api.js
@@ -2,7 +2,28 @@ async function fetchAPI(){
     const res = await fetch('/api/downloads');
     const json = await res.json();
 
-    console.log(json);
+    return json
 }
 
-fetchAPI();
+async function insertData(){
+    const OSdata = await fetchAPI();
+
+    if(OSdata.length){
+        const totalEl = document.getElementById('total');
+        let total = 0;
+
+        OSdata.forEach(os => {
+            const { name, downloads, detailed } = os;
+            total += downloads;
+    
+            const osEl = document.createElement('p');
+            osEl.textContent = `${ name }: ${ downloads }; ${ JSON.stringify(detailed) }`;
+    
+            document.body.appendChild(osEl);
+        });
+    
+        totalEl.textContent += total
+    }
+}
+
+insertData();
